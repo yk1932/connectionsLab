f342b65bd00c44fbb214473f7ca76e5b835f8c08
Yeji Kwon <yk1932@nyu.edu>
2022-02-23 23:56:55 +0400

    added logo to the tab

--- projectOne/versionTwo/app.js
+++ projectOne/versionTwo/app.js
@@ -9,11 +9,21 @@ var svgTwoFloor = document.getElementById('svgTwoFloor');
 var svgThreeFloor = document.getElementById('svgTwoFloor');
 var welcomeBookOne = document.getElementById('welcomeBookOne');
 var welcomeBookTwo = document.getElementById('welcomeBookTwo');
+var click = document.getElementById("click");
+var knock = document.getElementById("knock");
+var openDoor = document.getElementById("openDoor");
+var closeBook = document.getElementById("closeBook");
+var openBook = document.getElementById("openBook");
+
+
+
+
 
 //Interaction 1: Click on door to enter cafe
 
 home.addEventListener("mouseover", () =>{
-home.style.fill = 'beige';
+home.style.fill = '#433D37';
+knock.play();
 });
 
 home.addEventListener("mouseout", () =>{
@@ -24,12 +34,13 @@ home.addEventListener("click", () =>{
 svgOne.style.display = 'none';
 svgTwo.style.display = 'unset';
 document.getElementById("player").play();
+openDoor.play();
 });
 
 //Interaction 2: Click on floor to go back to home
 
 svgTwoFloor.addEventListener("mouseover", () =>{
-    svgTwoFloor.style.fill = 'beige';
+    svgTwoFloor.style.fill = 'white';
   });
 
 svgTwoFloor.addEventListener("mouseout", () =>{
@@ -44,8 +55,8 @@ svgOne.style.display = 'unset';
 //Interaction 3: Click on welcome book to browse
 
 welcomeBookOne.addEventListener("mouseover", () =>{
-    welcomeBookOne.style.fill = 'white';
-    welcomeBookTwo.style.fill = 'white';
+    welcomeBookOne.style.fill = '#B8C7C7';
+    welcomeBookTwo.style.fill = '#B8C7C7';
 
   });
 
@@ -57,11 +68,12 @@ welcomeBookOne.addEventListener("mouseout", () =>{
 welcomeBookOne.addEventListener("click", () =>{
     svgTwo.style.display = 'none';
     svgThree.style.display = 'unset';
+    openBook.play();
 });
 
 welcomeBookTwo.addEventListener("mouseover", () =>{
-    welcomeBookOne.style.fill = 'white';
-    welcomeBookTwo.style.fill = 'white';
+    welcomeBookOne.style.fill = '#B8C7C7';
+    welcomeBookTwo.style.fill = '#B8C7C7';
   });
 
 welcomeBookTwo.addEventListener("mouseout", () =>{
@@ -72,6 +84,7 @@ welcomeBookTwo.addEventListener("mouseout", () =>{
 welcomeBookTwo.addEventListener("click", () =>{
     svgTwo.style.display = 'none';
     svgThree.style.display = 'unset';
+    openBook.play()
 });
 
 //Interaction 4: Back button on svg three leads user back to cafe
@@ -79,7 +92,7 @@ welcomeBookTwo.addEventListener("click", () =>{
 var svgThreeBackButton = document.getElementById('svgThreeBackButton');
 
 svgThreeBackButton.addEventListener("mouseover", () =>{
-    svgThreeBackButton.style.fill = 'black';
+    svgThreeBackButton.style.fill = 'beige';
   });
 
 svgThreeBackButton.addEventListener("mouseout", () =>{
@@ -89,6 +102,7 @@ svgThreeBackButton.addEventListener("mouseout", () =>{
 svgThreeBackButton.addEventListener("click", () =>{
     svgTwo.style.display = 'unset';
     svgThree.style.display = 'none';
+    closeBook.play()
 });
 
 //Interaction 5: Click on any of the books on bookshelve opens svgFour
@@ -115,9 +129,11 @@ bookOne.addEventListener("mouseout", () =>{
     });
 
 bookOne.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#EF9262"
+    bookCover.style.fill = "#EF9262";
+    openBook.play();
     });
 
 //book two
@@ -131,9 +147,11 @@ bookTwo.addEventListener("mouseout", () =>{
     });
     
 bookTwo.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#FFD87E"
+    bookCover.style.fill = "#FFD87E";
+    openBook.play();
     });
 
 //book three
@@ -147,9 +165,11 @@ bookThree.addEventListener("mouseout", () =>{
     });
     
 bookThree.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#CAB177"
+    bookCover.style.fill = "#CAB177";
+    openBook.play();
     });
 
 //book four
@@ -163,9 +183,11 @@ bookFour.addEventListener("mouseout", () =>{
     });
     
 bookFour.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#B198C3"
+    bookCover.style.fill = "#B198C3";
+    openBook.play();
     });
 
 //book five
@@ -179,9 +201,11 @@ bookFive.addEventListener("mouseout", () =>{
     });
     
 bookFive.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#E89FCB"
+    bookCover.style.fill = "#E89FCB";
+    openBook.play();
     });
 
 //book six
@@ -195,9 +219,11 @@ bookSix.addEventListener("mouseout", () =>{
     });
     
 bookSix.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#BF8484"
+    bookCover.style.fill = "#BF8484";
+    openBook.play();
     });
 
 
@@ -212,9 +238,11 @@ bookSeven.addEventListener("mouseout", () =>{
     });
     
 bookSeven.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#BF8484"
+    bookCover.style.fill = "#BF8484";
+    openBook.play();
     });
     
 //book eight
@@ -228,16 +256,18 @@ bookEight.addEventListener("mouseout", () =>{
     });
     
 bookEight.addEventListener("click", () =>{
+    displayPoem();
     svgFour.style.display = 'unset';
     svgTwo.style.display = 'none';
-    bookCover.style.fill = "#7B80EE"
+    bookCover.style.fill = "#7B80EE";
+    openBook.play();
     });
 
 //Back Button
 var bookBackButton = document.getElementById("bookBackButton");
 
 bookBackButton.addEventListener("mouseover", () =>{
-    bookBackButton.style.fill="black";
+    bookBackButton.style.fill="beige";
     });
     
 bookBackButton.addEventListener("mouseout", () =>{
@@ -247,25 +277,83 @@ bookBackButton.addEventListener("mouseout", () =>{
 bookBackButton.addEventListener("click", () =>{
     svgFour.style.display = 'none';
     svgTwo.style.display = 'unset';
+    closeBook.play()
     });
 
-//API Fetch
+//API Fetch - Poem title and content
 
-window.addEventListener('load', function() {
-    console.log('page is loaded');
-    
-   fetch('https://poetrydb.org/title')
-   .then(response => response.json())
-   .then(data => {
-        //do something
-        for (var i=0; i<data.titles.length;i++){
-            this.console.log(data.titles[i]);
-        }
-   })
-   .catch(error => {
-       console.log("Error!!! : " + error);
-   })
+var poemTitle = document.getElementById('poemTitle');
+var poemAuthor = document.getElementById('poemAuthor');
+var poemContent = document.getElementById('poemContent');
+var poemContentTwo = document.getElementById('poemContentTwo');
+var poemContentThree = document.getElementById('poemContentThree');
+var poemContentFour = document.getElementById('poemContentFour');
+var poemContentFive = document.getElementById('poemContentFive');
+var poemContentSix = document.getElementById('poemContentSix');
+var poemContentSeven = document.getElementById('poemContentSeven');
+var poemContentEight = document.getElementById('poemContentEight');
+var poemContentNine = document.getElementById('poemContentNine');
+var poemContentTen = document.getElementById('poemContentTen');
 
-})
+
+
+function displayPoem() {
+    //Empty the content before displaying poem
+    poemTitle.innerHTML = '';
+    poemContent.innerHTML = '';
+    poemContentTwo.innerHTML = '';
+    poemContentThree.innerHTML = '';
+    poemContentFour.innerHTML = '';
+    poemContentFive.innerHTML = '';
+    poemContentSix.innerHTML = '';
+    poemContentSeven.innerHTML = '';
+    poemContentEight.innerHTML = '';
+    poemContentNine.innerHTML = '';
+    poemContentTen.innerHTML = '';
+    poemAuthor.innerHTML = '';
+
+    //Fetch API and display title and content
+    fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
+    .then(response => response.json())
+    .then(data => {
+        poemAuthor.innerHTML = data.quotes[0].author;
+
+        let lineOne = data.quotes[0].text.slice(0, 40);
+        let lineTwo = data.quotes[0].text.slice(40,80);
+        let lineThree = data.quotes[0].text.slice(80,120);
+        let lineFour = data.quotes[0].text.slice(120,160);
+        let lineFive = data.quotes[0].text.slice(160,200);
+        let lineSix = data.quotes[0].text.slice(200,240);
+        let lineSeven = data.quotes[0].text.slice(240,280);
+        let lineEight = data.quotes[0].text.slice(280,320);
+        let lineNine = data.quotes[0].text.slice(320,360);
+        let lineTen = data.quotes[0].text.slice(360,400);
+
+        poemContent.innerHTML = lineOne;
+        poemContentTwo.innerHTML = lineTwo;
+        poemContentThree.innerHTML = lineThree;
+        poemContentFour.innerHTML = lineFour;
+        poemContentFive.innerHTML = lineFive;
+        poemContentSix.innerHTML = lineSix;
+        poemContentSeven.innerHTML = lineSeven;
+        poemContentEight.innerHTML = lineEight;
+        poemContentNine.innerHTML = lineNine;
+        poemContentTen.innerHTML = lineTen;
+
+        })
+
+
+    .catch(error => {
+        console.log("Error!!! : " + error);
+    })
+}
+
+
+
+
+
+
+
+
 
 
